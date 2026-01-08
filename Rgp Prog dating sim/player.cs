5fae5357dac746ffb6d126f175b27ef30e2d6086
Kristianssorensen <97_stempel_popler@icloud.com>
2026-01-08 12:34:26 +0100

    min 1 max 5 in selection of stats

--- Rgp Prog dating sim/player.cs
+++ Rgp Prog dating sim/player.cs
@@ -12,21 +12,21 @@ public class player
         Console.WriteLine("Du skal sette dine stats. Højde, Charizzma, styrke. Skriv en værdig mellem 1-5, summen af dine stas må højest være 10");
         Console.WriteLine("Sæt højde: ");
         height = Convert.ToInt32(Console.ReadLine());
-        if (height > 5 && height < 1)
+        if (height > 5 || height < 1)
         {
             Console.WriteLine("hvæm tror du du er det må højest være 5 din bums! du er du på højde med mathias");
             height = 1;
         }
         Console.WriteLine("Sæt Charizzma: ");
         charisma = Convert.ToInt32(Console.ReadLine());
-        if (charisma > 5 && charisma < 1)
+        if (charisma > 5 || charisma < 1)
         {
             Console.WriteLine("hvæm tror du du er det må højest være 5 din bums! du har nu silases rizz!");
             charisma = 1;
         }
         Console.WriteLine("Sæt styrke: ");
         strength = Convert.ToInt32(Console.ReadLine());
-        if (strength > 5 && strength < 1)
+        if (strength > 5 || strength < 1)
         {
             Console.WriteLine("hvæm tror du du er det må højest være 5 din bums! du knækker som den tændstik du er!");
             strength= 1;
